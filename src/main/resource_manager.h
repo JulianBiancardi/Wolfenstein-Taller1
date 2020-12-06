023df6947884c970edc99cc29606a47c0705cec4
Santiago-Czop <czopsantiago@gmail.com>
2020-12-06 00:39:22 -0300

    [ADD] - Added get_image to ResourceManager

--- src/main/resource_manager.h
+++ src/main/resource_manager.h
@@ -1,20 +1,35 @@
 #ifndef RESOURCE_MANAGER_H
 #define RESOURCE_MANAGER_H
 
-#include <resource>
 #include <unordered_map>
+
+#include "resource.h"
+#include "sdl/image.h"
 
 class ResourceManager {
  private:
-  std::unordered_map<Resource> resources;
+  std::unordered_map<int, Resource> resources;
 
  public:
   ResourceManager();
   ~ResourceManager();
+
+  Image* get_image(int id);
 };
 
-ResourceManager::ResourceManager(/* args */) {}
+ResourceManager::ResourceManager() {}
 
 ResourceManager::~ResourceManager() {}
+
+Image* ResourceManager::get_image(int id) {
+  if (resources.find(id) == resources.end()) {
+    throw 1;  // TODO Throw KeyError
+  }
+  Resource& res = resources[id];
+  if (!res.is_loaded()) {
+    res.load();
+  }
+  return res.get_image();
+}
 
 #endif
