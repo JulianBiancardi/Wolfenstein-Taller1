0346057a85c685fc63c44b9e929e0a72ede4deef
Santiago-Czop <czopsantiago@gmail.com>
2020-12-06 03:01:29 -0300

    [UPDATE] - Added ResourceTests

--- src/tests/main_tests.cpp
+++ src/tests/main_tests.cpp
@@ -3,12 +3,14 @@
 #include "point_tests.h"
 #include "ray_tests.h"
 #include "rectangle_tests.h"
+#include "resource_tests.h"
 
 int main(int argc, char** argv) {
   matrix_tests();
   point_tests();
   ray_tests();
   rectangle_tests();
+  resource_tests();
   // gun_tests();
   return 0;
 }
