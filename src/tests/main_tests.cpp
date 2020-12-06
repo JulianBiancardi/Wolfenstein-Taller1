b1afe7f54cd89fd96d52aa7e36d55288a5e64f0d
Santiago-Czop <czopsantiago@gmail.com>
2020-12-06 02:50:59 -0300

    [UPDATE] - Added RectangleTests execution

--- src/tests/main_tests.cpp
+++ src/tests/main_tests.cpp
@@ -2,11 +2,13 @@
 #include "matrix_tests.h"
 #include "point_tests.h"
 #include "ray_tests.h"
+#include "rectangle_tests.h"
 
 int main(int argc, char** argv) {
   matrix_tests();
   point_tests();
   ray_tests();
+  rectangle_tests();
   // gun_tests();
   return 0;
 }
