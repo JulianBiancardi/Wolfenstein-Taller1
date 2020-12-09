a34dc0d5d022be8434f73c713c7ee0ffe67d1057
Santiago-Czop <czopsantiago@gmail.com>
2020-12-08 23:04:41 -0300

    [UPDATE] - Further solving includes

--- server/src/main/game/player.h
+++ server/src/main/game/player.h
@@ -1,9 +1,11 @@
 #ifndef WOLFENSTEIN_TALLER1_PLAYER_H
 #define WOLFENSTEIN_TALLER1_PLAYER_H
-#include "../config_loader.h"
+
 #include <iostream>
-#include "guns/gun.h"
 #include <list>
+
+#include "../config_loader.h"
+#include "guns/gun.h"
 #include "moveable.h"
 #include "shooter.h"
 
@@ -13,8 +15,9 @@ class Player : public Moveable, public Shooter {
   int shot_bullets;
   int health;
   int points;
-  //std::map<std::string, Gun> guns_bag;
-  std::list <Gun> guns_bag;
+  // std::map<std::string, Gun> guns_bag;
+  std::list<Gun> guns_bag;
+
  public:
   Player(Point origin, double angle, Map &game_map);
 
@@ -22,15 +25,14 @@ class Player : public Moveable, public Shooter {
 
   ~Player() {}
 
-//  void shoot_checker(double direction_angle);
+  //  void shoot_checker(double direction_angle);
 
   void shoot_player(Player &shot_player, Gun &gun) override;
 
-
   bool has_bullets(int amount);
-/*
-  void decrease_bullets(int amount);
-*/
+  /*
+    void decrease_bullets(int amount);
+  */
   void receive_damage(int amount);
 
   bool add_gun_if_hasnt(Gun gun);
@@ -44,4 +46,4 @@ class Player : public Moveable, public Shooter {
   int get_health();
 };
 
-#endif //WOLFENSTEIN_TALLER1_PLAYER_H
+#endif  // WOLFENSTEIN_TALLER1_PLAYER_H
