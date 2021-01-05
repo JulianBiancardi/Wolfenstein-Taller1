c5ac6cd0cfba612287846b3db5c75454e07cbd0f
Santiago-Czop <czopsantiago@gmail.com>
2021-01-05 16:47:59 -0300

    [UPDATE] - Refactored Food to work with Object

--- server/src/main/game/objects/items/food.cpp
+++ server/src/main/game/objects/items/food.cpp
@@ -1,22 +1,11 @@
 #include "food.h"
 
-Food::Food(Point center, int id) : Items(center, id) {
-  health_recovered = 10;
-}
+Food::Food(Point center, int id) : Item(center, id) { health_recovered = 10; }
 
-void Food::use(Player &user) {
-  user.add_health(health_recovered);
-}
+void Food::use(Player& user) { user.add_health(health_recovered); }
 
-Items *Food::copy() {
-  return new Food(center, id);
-}
+Item* Food::copy() { return new Food(position.get_origin(), id); }
 
-bool Food::can_be_used_by(Player &whom) {
-  return true;
-}
+bool Food::can_be_used_by(Player& whom) { return true; }
 
-void Food::remove() {
-
-}
-
+void Food::remove() {}
