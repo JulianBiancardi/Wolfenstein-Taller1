aa69fabf9c0a3d51a5963de3e253809cd75d34bf
Santiago-Czop <czopsantiago@gmail.com>
2021-01-05 18:00:45 -0300

    [UPDATE] - Refactored RocketLauncherItem to work with Object

---
+++ server/src/main/game/objects/items/rocket_launcher_item.cpp
@@ -0,0 +1,21 @@
+#include "rocket_launcher_item.h"
+
+#include "../../guns/rocket_launcher.h"
+
+RocketLauncherItem::RocketLauncherItem(Point center, int id)
+    : Item(center, id) {}
+
+void RocketLauncherItem::use(Player& user) {
+  // user.add_gun(gun);
+}
+
+Item* RocketLauncherItem::copy() {
+  return new RocketLauncherItem(position.get_origin(), id);
+}
+
+bool RocketLauncherItem::can_be_used_by(Player& whom) {
+  // return !whom.has_gun(gun);
+  return false;
+}
+
+void RocketLauncherItem::remove() {}
