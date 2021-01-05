d767019a87b032de0b1937c662d845f813ca1f85
Santiago-Czop <czopsantiago@gmail.com>
2021-01-05 16:26:24 -0300

    [UPDATE] - Refatored bullets to work with Object

--- server/src/main/game/objects/items/bullets.h
+++ server/src/main/game/objects/items/bullets.h
@@ -1,18 +1,18 @@
-#ifndef WOLFENSTEIN_TALLER1_BULLETS_H
-#define WOLFENSTEIN_TALLER1_BULLETS_H
+#ifndef BULLETS_H
+#define BULLETS_H
 
-#include "items.h"
+#include "item.h"
 
-class Bullets : public Items {
+class Bullets : public Item {
  public:
   Bullets(Point center, int id);
-  void use(Player &user) override;
-  Items *copy() override;
-  bool can_be_used_by(Player &whom) override;
+  void use(Player& user) override;
+  Item* copy() override;
+  bool can_be_used_by(Player& whom) override;
   void remove() override;
 
  private:
   int amount;
 };
 
-#endif //WOLFENSTEIN_TALLER1_BULLETS_H
+#endif
