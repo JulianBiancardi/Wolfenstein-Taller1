cd07bc3b8608f979ad07234c591b2980646dad9b
Santiago-Czop <czopsantiago@gmail.com>
2021-01-06 01:24:24 -0300

    [UPDATE] - Removed Player's inheritance from Shooter

--- server/src/main/game/player.cpp
+++ server/src/main/game/player.cpp
@@ -1,63 +1,50 @@
 #include "player.h"
+
 #include <cmath>
 
-Player::Player(Point origin, double angle, int id) :
-    shot_bullets(0), points(0), Moveable(origin, angle, id),
-    Shooter(origin, angle) {
+Player::Player(Point origin, double angle, int id)
+    : shot_bullets(0), points(0), Moveable(origin, angle, id) {
   max_health = ConfigLoader::player_health;
   health = max_health;
-  max_bullets = 100; // TODO Use config loader
+  max_bullets = 100;  // TODO Use config loader
   bullets = ConfigLoader::player_bullets;
 }
 
-Player::Player(double x, double y, double angle, int id) :
-    shot_bullets(0), points(0), Moveable(x, y, angle, id),
-    Shooter(Point(x, y), angle) {
+Player::Player(double x, double y, double angle, int id)
+    : shot_bullets(0), points(0), Moveable(x, y, angle, id) {
   max_health = ConfigLoader::player_health;
   health = max_health;
-  max_bullets = 100; // TODO Use config loader
+  max_bullets = 100;  // TODO Use config loader
   bullets = ConfigLoader::player_bullets;
 }
 
-void Player::shoot_player(Player &shot_player, Gun &gun) {
+/*void Player::shoot_player(Player &shot_player, Gun &gun) {
 //  shoot_checker();
   int damage_got = gun.shoot(bullets);
   shot_player.receive_damage(damage_got);
-}
+}*/
 
-bool Player::has_bullets(int amount) {
-  return (bullets >= amount);
-}
+bool Player::has_bullets(int amount) { return (bullets >= amount); }
 /*
 void Player::decrease_bullets(int amount) {
   bullets -= amount;
 }
 */
-void Player::receive_damage(int amount) {
-  health -= amount;
-}
+void Player::receive_damage(int amount) { health -= amount; }
 
-void Player::add_gun(const Gun gun) {
-  guns_bag.push_back(gun);
-}
+void Player::add_gun(const Gun gun) { guns_bag.push_back(gun); }
 
-bool Player::has_gun(const Gun gun) {
-  return false;
-}
+bool Player::has_gun(const Gun gun) { return false; }
 /*
 bool Player::add_gun_if_hasnt(Gun gun) {
   bool found =
-      false;//(std::find(guns_bag.begin(), guns_bag.end(), gun) != guns_bag.end());
-  if (!found) {
-    guns_bag.push_back(gun);
-    return true;
+      false;//(std::find(guns_bag.begin(), guns_bag.end(), gun) !=
+guns_bag.end()); if (!found) { guns_bag.push_back(gun); return true;
   }
   return false;
 }
 */
-void Player::add_points(int amount) {
-  points += amount;
-}
+void Player::add_points(int amount) { points += amount; }
 
 void Player::add_bullets(int amount) {
   if (bullets + amount < max_bullets) {
@@ -86,16 +73,12 @@ bool Player::add_health_if_hass_less(int amount, int less_than) {
 
 void Player::decrease_health(int amount) {
   if (health - amount <= 0) {
-    health = 0; //Is_dead
+    health = 0;  // Is_dead
   } else {
     health -= amount;
   }
 }
 
-bool Player::is_full_health() {
-  return health == max_health;
-}
+bool Player::is_full_health() { return health == max_health; }
 
-int Player::get_health() {
-  return health;
-}
+int Player::get_health() { return health; }
--- server/src/main/game/player.h
+++ server/src/main/game/player.h
@@ -2,14 +2,14 @@
 #define WOLFENSTEIN_TALLER1_PLAYER_H
 
 #include <iostream>
-#include <list>
+#include <vector>
 
 #include "../config_loader.h"
 #include "guns/gun.h"
 #include "moveable.h"
 #include "shooter.h"
 
-class Player : public Moveable, public Shooter {
+class Player : public Moveable {
  private:
   int max_bullets;
   int bullets;
@@ -18,15 +18,17 @@ class Player : public Moveable, public Shooter {
   int health;
   int points;
   // std::map<std::string, Gun> guns_bag;
-  std::list<Gun> guns_bag;
+  std::vector<Gun> guns_bag;
+  Gun* active_gun;
 
  public:
   Player(Point origin, double angle, int id);
   Player(double x, double y, double angle, int id);
   ~Player() {}
 
+  void shoot(Gun& gun, Map& map);
   // void shoot_checker(double direction_angle);
-  void shoot_player(Player &shot_player, Gun &gun) override;
+  // void shoot_player(Player& shot_player, Gun& gun) override;
   bool has_bullets(int amount);
   // void decrease_bullets(int amount);
   void receive_damage(int amount);
