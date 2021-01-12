575a3599c7c4eda271811374ce5059a25c80634c
Santiago-Czop <czopsantiago@gmail.com>
2021-01-12 17:11:35 -0300

    [UPDATE] - Changed some names from Event to Packet_t

--- server/src/main/events/event_building.cpp
+++ server/src/main/events/event_building.cpp
@@ -1,14 +1,15 @@
 #include "event_building.h"
 
-Event build_move_event(int player_id, Point where) {
+packet_t build_move_event(int player_id, Point where) {
   PointData point = {.x = where.getX(), .y = where.getY()};
-  Event event = {.type = 1, .player_id = player_id, .data={.point = point}};
+  packet_t event = {
+      .type = 1, .player_id = player_id, .data = {.point = point}};
 
   return event;
 }
 
-Event build_grab_event(int player_id, int what) {
-  Event event = {.type = 2, .player_id = player_id, .data={.item = what}};
+packet_t build_grab_event(int player_id, int what) {
+  packet_t event = {.type = 2, .player_id = player_id, .data = {.item = what}};
 
   return event;
 }
