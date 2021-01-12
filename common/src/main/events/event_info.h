81056b7071f5da093caca213e58e0d96db473a57
JuanStenghele <jstenghele@fi.uba.ar>
2021-01-12 16:27:14 -0300

    [ADD] - Added shoot event.

--- common/src/main/events/event_info.h
+++ common/src/main/events/event_info.h
@@ -1,18 +1,24 @@
 #ifndef WOLFENSTEIN_TALLER1_SERVER_SRC_MAIN_EVENTS_EVENT_H_
 #define WOLFENSTEIN_TALLER1_SERVER_SRC_MAIN_EVENTS_EVENT_H_
 
-// TODO Move to common space
 typedef struct PointData {
   double x;
   double y;
 } PointData;
 
+typedef struct ShootData {
+  int gun;
+  double damage_done; // May use float
+  int enemy_shot; // -1 if no enemy was shot
+} PointData;
+
 typedef struct Event {
-  int type; // 1 MOVE, 2 GRAB
+  int type; // 1 MOVE, 2 GRAB, 3 SHOOT
   int player_id;
   union data {
     PointData point; // MOVE
     int item; // GRAB
+    ShootData shot; // SHOOT
   } data;
 } Event;
 
