6efa882ed879b0c5673e97554ac8a020d416f07f
Santiago-Czop <czopsantiago@gmail.com>
2021-01-12 22:02:06 -0300

    [ADD] Added Threads for the Server. Added a File to keep track of the Packets

---
+++ server/src/main/threads/send_to_client_thread.cpp
@@ -0,0 +1,37 @@
+#include "send_to_client_thread.h"
+
+#include <syslog.h>
+
+#include <utility>
+
+SendToClientThread::SendToClientThread(Socket& connected_socket,
+                                       BlockingQueue<packet_t>* events_queue)
+    : connected_socket(connected_socket),
+      events_queue(events_queue),
+      allowed_to_run(true),
+      running(false) {}
+
+SendToClientThread::~SendToClientThread() {}
+
+void SendToClientThread::force_stop() {
+  allowed_to_run = false;
+  packet_t disconnection_packet;
+  // TODO Fill with disconnection info
+  events_queue->enqueue(disconnection_packet);
+}
+
+void SendToClientThread::run() {
+  try {
+    running = true;
+    while (allowed_to_run) {
+      packet_t packet;
+      events_queue->dequeue(packet);
+      connected_socket.send((char*)&packet, sizeof(packet));
+    }
+    running = false;
+  } catch (const std::exception& e) {
+    syslog(LOG_ERR, "[Error] SendToServerThread - Error: %s", e.what());
+  } catch (...) {
+    syslog(LOG_ERR, "[Error] SendToServerThread - Unknown error");
+  }
+}
---
+++ server/src/main/threads/send_to_client_thread.h
@@ -0,0 +1,25 @@
+#ifndef SEND_THREAD_H
+#define SEND_THREAD_H
+
+#include "../../../../common/src/main/data_structures/blocking_queue.h"
+#include "../../../../common/src/main/packets/packet.h"
+#include "../../../../common/src/main/socket/socket.h"
+#include "../../../../common/src/main/threads/thread.h"
+
+class SendToClientThread : public Thread {
+ private:
+  BlockingQueue<packet_t>* events_queue;
+  Socket& connected_socket;
+  bool allowed_to_run;
+  bool running;
+
+ public:
+  SendToClientThread(Socket& connected_socket,
+                     BlockingQueue<packet_t>* events_queue);
+  ~SendToClientThread();
+
+  void force_stop();
+  void run() override;
+};
+
+#endif
