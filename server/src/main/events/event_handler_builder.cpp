26ac37bab02d186d92ec48d7676e1de376d6f098
JuanStenghele <jstenghele@fi.uba.ar>
2021-01-14 21:04:08 -0300

    [UPDATE] - Remade handler building methods.

--- server/src/main/events/event_handler_builder.cpp
+++ server/src/main/events/event_handler_builder.cpp
@@ -10,11 +10,24 @@ Player& EventHandlerBuilder::get_doer(const packet_t& event,
   return players.at(event.player_id);
 }
 
-// TODO Remake
-EventHandler* EventHandlerBuilder::move_event(Player& doer,
-                                              double x,
-                                              double y) {
-  return new MoveHandler(doer, Point(x, y));
+EventHandler* move_event(Player& who, const packet_t& event) {
+  return new MoveHandler(who, Point(event.data.point.x, event.data.point.y));
+}
+
+EventHandler* shot_hit_event(Player& who,
+                             Player& enemy_hit,
+                             const packet_t& event) {
+  int bullets_shot = event.data.shot.bullets_shot;
+  double damage_done = event.data.shot.damage_done;
+  return new ShotHitHandler(who, enemy_hit, damage_done, bullets_shot);
+}
+
+EventHandler* shot_miss_event(Player& who, const packet_t& event) {
+  return new ShotMissHandler(who, event.data.bullets_shot);
+}
+
+EventHandler* change_gun_event(Player& who, const packet_t& event) {
+  return new ChangeGunHandler(who, event.data.gun);
 }
 
 EventHandler* EventHandlerBuilder::build(const packet_t& event,
@@ -24,15 +37,12 @@ EventHandler* EventHandlerBuilder::build(const packet_t& event,
 
   //TODO Use constants
   switch (event.type) {
-    case 1: return move_event(doer, event.data.point.x, event.data.point.y);
-    case 3: {
-      int bullets_shot = event.data.shot.bullets_shot;
-      Player& enemy_hit = players.at(event.data.shot.enemy_shot);
-      double damage_done = event.data.shot.damage_done;
-      return new ShotHitHandler(doer, enemy_hit, damage_done, bullets_shot);
-    }
-    case 5: return new ChangeGunHandler(doer, event.data.gun);
-    case 6: return new ShotMissHandler(doer, event.data.bullets_shot);
+    case 1: return move_event(doer, event);
+    case 3:
+      return shot_hit_event(doer,
+                            players.at(event.data.shot.enemy_shot).event);
+    case 5: return change_gun_event(doer, event);
+    case 6: return shot_miss_event(doer, event);
     default: throw -1; //TODO Throw ours exception
   }
 }
