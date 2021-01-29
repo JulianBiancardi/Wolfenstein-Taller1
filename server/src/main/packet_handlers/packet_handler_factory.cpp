8532d517a84b80e2b54ab3a4480ac32418faded4
Santiago-Czop <czopsantiago@gmail.com>
2021-01-28 22:25:52 -0300

    [ADD] - Added new types to Handler Factory

--- server/src/main/packet_handlers/packet_handler_factory.cpp
+++ server/src/main/packet_handlers/packet_handler_factory.cpp
@@ -3,13 +3,19 @@
 #include "../../../../common/src/main/packets/packet.h"
 #include "../../../../common/src/main/packets/packet_error.h"
 #include "change_gun_handler.h"
+#include "join_match_handler.h"
 #include "move_handler.h"
 #include "request_matches_handler.h"
+#include "request_new_match_handler.h"
 
 PacketHandler* PacketHandlerFactory::build(Packet& packet) {
   switch (packet.get_type()) {
     case REQUEST_MATCHES:
       return new RequestMatchesHandler();
+    case REQUEST_NEW_MATCH:
+      return new RequestNewMatchHandler();
+    case JOIN_MATCH:
+      return new JoinMatchHandler();
       // TODO FIX This once all have been defined
     /*case MOVE_PACKET:
       return new MoveHandler();
