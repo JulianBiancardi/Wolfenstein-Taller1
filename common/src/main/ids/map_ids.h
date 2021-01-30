e24e268a9b696c1fcf8d9a054fcbd97900fbcba6
Santiago-Czop <czopsantiago@gmail.com>
2021-01-30 16:28:54 -0300

    [ADD] - Added map_ids file

---
+++ common/src/main/ids/map_ids.h
@@ -0,0 +1,44 @@
+#ifndef MAP_IDS_H
+#define MAP_IDS_H
+
+// Walls
+#define AIR 0
+#define ROCK_WALL 1
+#define WOOD_WALL 2
+
+// Players
+#define SPAWN_POINT 20
+#define DOG 21
+#define GUARD 22
+#define SS 23
+#define OFFICER 24
+#define MUTANT 25
+
+// Guns
+#define KNIFE 30
+#define PISTOL 31
+#define MACHINE_GUN 32
+#define CHAIN_GUN 33
+#define BAZOOKA 34
+
+// Items
+#define FOOD 40
+#define MEDKIT 41
+#define BLOOD 42
+#define BULLETS 43
+#define CROSS 44
+#define CUP 45
+#define CHEST 46
+#define CROWN 47
+#define KEY 48
+
+// Solid Objects
+#define PILLAR 50
+#define TABLE 51
+#define BARREL 52
+
+// Define Non-Solid Objects
+#define WATER 60
+#define FLOOR_BLOOD 61
+
+#endif
