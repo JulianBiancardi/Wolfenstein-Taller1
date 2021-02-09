7648c9f45d1464b4099f4bb71143d42437fc66ff
JuanStenghele <jstenghele@fi.uba.ar>
2021-02-08 21:37:25 -0300

    [ADD] - Finished rocket's logic in server. Updated handlers and match's methods.

--- server/src/main/packet_handlers/rocket_move_handler.cpp
+++ server/src/main/packet_handlers/rocket_move_handler.cpp
@@ -1,11 +1,38 @@
 #include "rocket_move_handler.h"
 
+#include "../../../../common/src/main/packets/packing.h"
+
 RocketMoveHandler::RocketMoveHandler() {}
 
 RocketMoveHandler::~RocketMoveHandler() {}
 
 void RocketMoveHandler::handle(Packet& packet, ClientManager& client_manager,
                                MatchManager& match_manager) {
+  unsigned char type;
+  unsigned char rocket_id;
+  unsigned char match_id;
+  unsigned int player_id;
+  unpack(packet.get_data(), "CICI", &type, &rocket_id, &match_id, &player_id);
 
+  Match& match = match_manager.get_match(match_id);
+
+  const std::unordered_set<unsigned int>& clients = match.get_players_ids();
+
+  client_manager.send_to_all(clients, packet);
+
+  if (!match.move_rocket(rocket_id)) {
+    std::unordered_map<unsigned int, unsigned char> players_exploded =
+        match.explode_rocket(rocket_id, player_id);
+
+    unsigned char data[ROCKET_EXPLODE_SIZE];
+    size_t size = pack(data, "CI", ROCKET_EXPLODE, &rocket_id);
+
+    for (auto player_exploded : players_exploded) {
+      size += pack(data, "IC", &player_exploded.first, &player_exploded.second);
+    }
+
+    Packet explosion_packet(size, data);
+
+    client_manager.send_to_all(clients, explosion_packet);
+  }
 }
-
--- server/src/main/threads/clock/rocket_controller.cpp
+++ server/src/main/threads/clock/rocket_controller.cpp
@@ -4,14 +4,17 @@
 
 RocketController::RocketController(BlockingQueue<Packet>& queue,
                                    unsigned char match_id,
-                                   unsigned int rocket_id)
-    : TimedEvent(1, queue, match_id), rocket_id(rocket_id) {}
+                                   unsigned int rocket_id,
+                                   unsigned int player_id)
+    : TimedEvent(1, queue, match_id), rocket_id(rocket_id),
+      player_id(player_id) {}
 
 RocketController::~RocketController() {}
 
 void RocketController::execute() {
   unsigned char data[ROCKET_MOVE_SIZE];
-  size_t size = pack(data, "CIC", ROCKET_MOVE, &rocket_id, &match_id);
+  size_t size = pack(data, "CICI", ROCKET_MOVE, &rocket_id, &match_id,
+                     &player_id);
   Packet packet(size, data);
 
   reception_queue.enqueue(packet);
